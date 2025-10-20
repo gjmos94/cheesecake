1ad40ee65c32b770c5377e9bb2e4f9082d0dd2da
gjmos94 <gjmos94@outlook.com>
2025-10-20 02:54:52 -0500

    Section CSS updates and Firebase implementation

---
+++ public/js/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCjTa_z6hs1T-QQx0HZFTjCBgJNtfD57YA",
+  authDomain: "cheesecake-fa21f.firebaseapp.com",
+  projectId: "cheesecake-fa21f",
+  storageBucket: "cheesecake-fa21f.firebasestorage.app",
+  messagingSenderId: "364828237277",
+  appId: "1:364828237277:web:d53d95c5de300334e4aa34"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
